7421d2470b6b097a9dbd0e19ff4807b32eab6b1d
박정윤 <parkjeong-yoon@bagjeong-yun-ui-MacBookPro.local>
2020-12-25 17:54:11 +0900

    add redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
+import { Provider } from  'react-redux';
+
 import './App.css';
 // import Header from './components/Header.js';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Profile from './components/Profile.js';
 import Information from './components/Information.js';
 import Footer from './components/Footer.js';
 
+import createStore from './store';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
 function App() {
   
   return (
-    <div className="App">
-      {/* <Header /> */}
-      <div className="Contents">
-        <Profile />
-        <Information />
+    <Provider store={store}>
+      <div className="App">
+        {/* <Header /> */}
+        <div className="Contents">
+          <Profile />
+          <Information />
+        </div>
         <Footer />
       </div>
-    </div>
+    </Provider>  
   );
 }
  
--- src/components/Information.js
+++ src/components/Information.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useSelector, useDispatch } from  'react-redux';
+import * as actions from  '../actions/count';
 import '../css/Information.css';
 import phone from '../img/Phone.png';
 import email from '../img/Email.png';
@@ -19,11 +21,30 @@ import Project4 from '../img/project4.png';
 import Windows from '../img/Windows.png';
 import Linux from '../img/Linux.png';
 
-function Information() {
+const Information = () => {
 
     const tab = ["Profile", "Skills", "Contact"];
-    const [status, setStatus] = useState(1);
-    const [show, setShow] = useState();
+    const [status, setStatus] = useState(0);
+    const [show, setShow] = useState(0);
+
+    const dispatch = useDispatch();
+    const [localCount, setLocalCount] = useState(0);
+
+    const { count:storeCount } = useSelector((state) => state.count);
+
+    useEffect(() => {
+        console.log('Component did mount');
+    }, []);
+
+    const increaseCount = useCallback(() => {
+        setLocalCount(localCount + 1);
+        dispatch(actions.increaseCount());
+    }, [localCount, dispatch]);
+
+    const decreaseCount = useCallback(() => {
+        setLocalCount(localCount - 1);
+        dispatch(actions.decreaseCount());
+    }, [localCount, dispatch]);
 
     function tabChange(idx){
         setStatus(idx);
@@ -56,25 +77,48 @@ function Information() {
                                 <img className="profileImg" src={profile} alt=""/>
                                 <h3>박정윤</h3>
                                 <h3>2001-09-22(만 19세)</h3>
-                                <h3>서울디지텍고등학교 유비쿼터스과 졸업</h3>
-                                <h3>달리셔스(주) 개발팀 2020-11 ~ 재직중</h3>
+                                <h3>달리셔스(주)<br/>개발팀 2019-11 ~ 재직중</h3>
                             </div>
                             
                         </div>
                         <h1 onClick={() => showChange(1)} className={show==1 ? "active" : ""}>Project. <span>{show==1 ? "-" : "+"}</span></h1>
-                        <div className={show==1 ? "show" : "hidden"}>
+                        <div className={(show==1 ? "show" : "hidden") + " ProjectWrap"}>
                             <div className="ProjectBox">
                                 <h2>달리셔스 BackOffice 제작</h2>
                                 <h3>2019-12 ~ ing</h3>
-                                <h3>HTML5, CSS3, Sass, PHP, MySQL, Ajax</h3>
+                                <div className="icon_box">
+                                    <img src={HTML} className="icon" alt=""/>
+                                    <img src={CSS} className="icon" alt=""/>
+                                    <img src={JS} className="icon" alt=""/>
+                                    <img src={jQuery} className="icon" alt=""/>
+                                    <img src={PHP} className="icon" alt=""/>
+                                    <img src={Sass} className="icon" alt=""/>
+                                    <img src={MySQL} className="icon" alt=""/>
+                                </div>
                                 <img src={Project1} alt=""/>
+                                <h4>
+                                    BackOffice 개발.<br/>
+                                    상품, 식단, 주문, 컨텐츠 등등 <br/> 운영의 전반적인 부분을 관리하는 사이트    
+                                </h4>
                             </div>
                             <div className="ProjectBox">
-                                <h2>달리셔스 메이커스용 모바일 웹</h2>
+                                <h2>메이커스용 모바일 웹</h2>
                                 <h2>2020-10 ~ ing</h2>
-                                <h3>HTML5, CSS3, Sass, PHP, MySQL, Ajax</h3>
+                                <div className="icon_box">
+                                    <img src={HTML} className="icon" alt=""/>
+                                    <img src={CSS} className="icon" alt=""/>
+                                    <img src={JS} className="icon" alt=""/>
+                                    <img src={jQuery} className="icon" alt=""/>
+                                    <img src={PHP} className="icon" alt=""/>
+                                    <img src={Sass} className="icon" alt=""/>
+                                    <img src={MySQL} className="icon" alt=""/>
+                                </div>
                                 <img src={Project3} className="Mobile" alt=""/>
                                 <img src={Project4} className="Mobile" alt=""/>
+                                <h4>
+                                    음식점에서 주문, 식단을 체크하고 준비상태로 바꾸는 페이지 개발<br/>
+                                    준비된 메뉴를 오른쪽으로 밀면 상태가 변경됨
+                                </h4>
                             </div>
 
                         </div>
@@ -112,7 +156,8 @@ function Information() {
                 <div className={(status==2 ? " display " : "")+"Contact tab"}>
                     <div className="card">
                         <span className="Info"><img src={phone} alt=""/><span>Phone: 010-4139-8132</span></span>
-                        <span className="Info"><img src={email} alt=""/><span>E-mail: pjy20010922@gmail.com</span></span>
+                        <span className="Info"><img src={email} alt=""/><span><a href="mailto:pjy20010922@gmail.com" target="_blank">E-mail: pjy20010922@gmail.com</a></span></span>
+                        <span className="Info"><img src={GitHub} alt=""/><span><a href="https://github.com/JeongYoonPark" target="_blank">GitHub: https://github.com/JeongYoonPark</a></span></span>
                     </div>
                 </div>
                 
