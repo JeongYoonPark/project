132ec4de1dff5798e18e82f8b3bd837f1b9d9066
박정윤 <parkjeong-yoon@bagjeong-yun-ui-MacBookPro.local>
2020-12-29 16:34:41 +0900

    design edit

--- src/components/Information.js
+++ src/components/Information.js
@@ -20,12 +20,12 @@ import Project3 from '../img/project3.png';
 import Project4 from '../img/project4.png';
 import Windows from '../img/Windows.png';
 import Linux from '../img/Linux.png';
+import Redux from '../img/redux.png';
 
 const Information = () => {
 
-    const tab = ["Profile", "Skills", "Contact"];
-    const [status, setStatus] = useState(0);
-    const [show, setShow] = useState(0);
+    const tab = ["Profile", "Project", "Contact"];
+    const [status, setStatus] = useState(1);
 
     const dispatch = useDispatch();
     const [localCount, setLocalCount] = useState(0);
@@ -45,20 +45,17 @@ const Information = () => {
         dispatch(actions.decreaseCount());
     }, [localCount, dispatch]);
 
+    const backgroundBlack = useCallback(() => {
+
+    })
+
     function tabChange(idx){
         setStatus(idx);
-    }
-    function showChange(idx){
-        if(show==idx){
-            setShow();
-        }else{
-            setShow(idx);
-        }
     }
     
     return (
         <section className="Information">
-            <>
+            {/* <>
       {'Function component'}
       <div>
         <div>
@@ -74,7 +71,7 @@ const Information = () => {
       <div onClick={decreaseCount}>
         {'-'}
       </div>
-    </>
+    </> */}
             <div className="head">
                 {tab.map((name, idx) => {
                     return(
@@ -87,86 +84,79 @@ const Information = () => {
             <div className="tab_box">
                 <div className={(status==0 ? " display " : "")+"Profile tab"}>
                     <div className="introduce">
-                        <h1 onClick={() => showChange(0)} className={show==0 ? "active" : ""}>Who am I.<span>{show==0 ? "-" : "+"}</span></h1>
-                        <div className={(show==0 ? "show" : "hidden") + " profile"}>
+                        <h1 className="active">Who am I.</h1>
+                        <div className="profile">
                             <div className="InfoBox">
                                 <img className="profileImg" src={profile} alt=""/>
                                 <h3>박정윤</h3>
                                 <h3>2001-09-22(만 19세)</h3>
                                 <h3>달리셔스(주)<br/>개발팀 2019-11 ~ 재직중</h3>
                             </div>
-                            
-                        </div>
-                        <h1 onClick={() => showChange(1)} className={show==1 ? "active" : ""}>Project. <span>{show==1 ? "-" : "+"}</span></h1>
-                        <div className={(show==1 ? "show" : "hidden") + " ProjectWrap"}>
-                            <div className="ProjectBox">
-                                <h2>달리셔스 BackOffice 제작</h2>
-                                <h3>2019-12 ~ ing</h3>
-                                <div className="icon_box">
-                                    <img src={HTML} className="icon" alt=""/>
-                                    <img src={CSS} className="icon" alt=""/>
-                                    <img src={JS} className="icon" alt=""/>
-                                    <img src={jQuery} className="icon" alt=""/>
-                                    <img src={PHP} className="icon" alt=""/>
-                                    <img src={Sass} className="icon" alt=""/>
-                                    <img src={MySQL} className="icon" alt=""/>
+                            <div className="container">
+                                <div className="SkillList">
+                                    <h2>FRONT-END</h2>
+                                    <div className="front skill_box">
+                                        <span><img src={HTML} alt=""/></span>
+                                        <span><img src={CSS} alt=""/></span>
+                                        <span><img src={JS} alt=""/></span>
+                                        <span><img src={jQuery} alt=""/></span>
+                                        <span><img src={ReactImg} alt=""/></span>
+                                        <span><img src={Sass} alt=""/></span>
+                                        <span> <img src={Redux} className="icon" alt=""/></span>
+                                    </div>
+                                    <h2>BACK-END</h2>
+                                    <div className="back skill_box">
+                                        <span><img src={PHP} alt=""/></span>
+                                        <span><img src={MySQL} alt=""/></span>
+                                        <span><img src={Node} alt=""/></span>
+                                    </div>
+                                    <h2>ETC</h2>
+                                    <div className="tool skill_box">
+                                        <span><img src={Windows} alt=""/></span>
+                                        <span><img src={Linux} alt=""/></span>
+                                        <span><img src={GitHub} alt=""/></span>
+                                    </div>
                                 </div>
-                                <img src={Project1} alt=""/>
-                                <h4>
-                                    BackOffice 개발.<br/>
-                                    상품, 식단, 주문, 컨텐츠 등등 <br/> 운영의 전반적인 부분을 관리하는 사이트    
-                                </h4>
                             </div>
-                            <div className="ProjectBox">
-                                <h2>메이커스용 모바일 웹</h2>
-                                <h2>2020-10 ~ ing</h2>
-                                <div className="icon_box">
-                                    <img src={HTML} className="icon" alt=""/>
-                                    <img src={CSS} className="icon" alt=""/>
-                                    <img src={JS} className="icon" alt=""/>
-                                    <img src={jQuery} className="icon" alt=""/>
-                                    <img src={PHP} className="icon" alt=""/>
-                                    <img src={Sass} className="icon" alt=""/>
-                                    <img src={MySQL} className="icon" alt=""/>
-                                </div>
-                                <img src={Project3} className="Mobile" alt=""/>
-                                <img src={Project4} className="Mobile" alt=""/>
-                                <h4>
-                                    음식점에서 주문, 식단을 체크하고 준비상태로 바꾸는 페이지 개발<br/>
-                                    준비된 메뉴를 오른쪽으로 밀면 상태가 변경됨
-                                </h4>
-                            </div>
-
                         </div>
-
                     </div>  
                 </div>
-                <div className={(status==1 ? " display " : "")+"Skills tab"}>
-                    <div className="container">
-                        <div className="SkillList">
-                            <h2>FRONT-END</h2>
-                            <div className="front skill_box">
-                                <span><img src={HTML} alt=""/></span>
-                                <span><img src={CSS} alt=""/></span>
-                                <span><img src={JS} alt=""/></span>
-                                <span><img src={jQuery} alt=""/></span>
-                                <span><img src={ReactImg} alt=""/></span>
-                                <span><img src={Sass} alt=""/></span>
-                            </div>
-                            <h2>BACK-END</h2>
-                            <div className="back skill_box">
-                                <span><img src={PHP} alt=""/></span>
-                                <span><img src={MySQL} alt=""/></span>
-                                <span><img src={Node} alt=""/></span>
-                            </div>
-                            <h2>ETC</h2>
-                            <div className="tool skill_box">
-                            <span><img src={Windows} alt=""/></span>
-                            <span><img src={Linux} alt=""/></span>
-                            <span><img src={GitHub} alt=""/></span>
-                            </div>
-
+                <div className={(status==1 ? " display " : "")+"Project tab"}>
+                    <div className="ProjectBox">
+                        <h2>달리셔스 BackOffice 제작</h2>
+                        <h3>2019-12 ~ ing</h3>
+                        <div className="icon_box">
+                            <img src={HTML} className="icon" alt=""/>
+                            <img src={CSS} className="icon" alt=""/>
+                            <img src={JS} className="icon" alt=""/>
+                            <img src={jQuery} className="icon" alt=""/>
+                            <img src={PHP} className="icon" alt=""/>
+                            <img src={Sass} className="icon" alt=""/>
+                            <img src={MySQL} className="icon" alt=""/>
                         </div>
+                        <img src={Project1} alt=""/>
+                        <h4>
+                            BackOffice 개발.<br/>
+                            상품, 식단, 주문, 컨텐츠 등등 <br/> 운영의 전반적인 부분을 관리하는 사이트    
+                        </h4>
+                    </div>
+                    <div className="ProjectBox">
+                        <h2>모바일 웹</h2>
+                        <h2>2020-10 ~ ing</h2>
+                        <div className="icon_box">
+                            <img src={HTML} className="icon" alt=""/>
+                            <img src={CSS} className="icon" alt=""/>
+                            <img src={JS} className="icon" alt=""/>
+                            <img src={jQuery} className="icon" alt=""/>
+                            <img src={PHP} className="icon" alt=""/>
+                            <img src={Sass} className="icon" alt=""/>
+                            <img src={MySQL} className="icon" alt=""/>
+                        </div>
+                        <img src={Project3} className="Mobile" alt=""/>
+                        <img src={Project4} className="Mobile" alt=""/>
+                        <h4>
+                            주문, 식단을 체크하고 준비상태로 바꾸는 사이트 
+                        </h4>
                     </div>
                 </div>
                 <div className={(status==2 ? " display " : "")+"Contact tab"}>
