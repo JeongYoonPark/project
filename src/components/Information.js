ae071f735d7d2120cffd94d59748c5abb3c5b4fe
ParkJeongYoon2 <pjy20010922@gmail.com>
2022-01-07 13:55:47 +0900

    edit information

--- src/components/Information.js
+++ src/components/Information.js
@@ -97,7 +97,7 @@ const Information = () => {
                                 <img className="profileImg" src={profile} alt=""/>
                                 <h3>박정윤</h3>
                                 <h3>2001-09-22(만 20세)</h3>
-                                <h3>달리셔스(주)<br/>개발팀 2019-11 ~ 재직중</h3>
+                                <h3>달리셔스(주)<br/>개발팀 2019-11 ~ 2021-03</h3>
                             </div>
                             <div className="container">
                                 <div className="SkillList">
